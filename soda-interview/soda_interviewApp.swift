0d75bf7adffb73efb4cecc7f3d575caa383dc4b4
Chinmay Anand <chinmayanand896@gmail.com>
2025-12-06 20:49:35 +0900

    Initial Commit

---
+++ soda-interview/soda_interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  soda_interviewApp.swift
+//  soda-interview
+//
+//  Created by Chinmay Anand on 2025/12/06.
+//
+
+import SwiftUI
+
+@main
+struct soda_interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ soda-interviewTests/soda_interviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  soda_interviewTests.swift
+//  soda-interviewTests
+//
+//  Created by Chinmay Anand on 2025/12/06.
+//
+
+import Testing
+@testable import soda_interview
+
+struct soda_interviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
